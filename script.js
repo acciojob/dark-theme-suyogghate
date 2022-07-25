7e8fd975627771d3bc53d098d30a121ddd8d2493
Ghate Suyog <suyogghateminks@gmail.com>
2022-07-25 18:04:25 +0530

    changes completed

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
 
+const main = document.getElementById('app');
+const btn = document.getElementById('swap');
+
+btn.addEventListener('click', () => {
+    if(main.classList.contains("night")){
+        main.classList.remove("night");
+        btn.classList.remove("button_night");
+    }
+    else{
+        main.classList.add("night");
+        btn.classList.add("button_night");
+    }
+});
